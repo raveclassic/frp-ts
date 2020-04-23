9bd0cf1e60b8fbac0785918386806a8d70f5a190
raveclassic <raveclassic@gmail.com>
2020-04-23 17:23:04 +0300

    refactor: export clock from index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export * as source from './source';
-export * as observable from './observable';
+export * as clock from './clock';
 export * as emitter from './emitter';
+export * as observable from './observable';
 export * as producer from './producer';
+export * as source from './source';
