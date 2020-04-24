c18782e67d02ea6d810784d5c270e212e47e78a3
raveclassic <raveclassic@gmail.com>
2020-04-24 17:04:35 +0300

    style: fix prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
 		'eslint:recommended',
 		'plugin:@typescript-eslint/eslint-recommended',
 		'plugin:@typescript-eslint/recommended',
-		'plugin:@typescript-eslint/recommended-requiring-type-checking'
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
 	],
 	rules: {
 		'@typescript-eslint/member-delimiter-style': [
