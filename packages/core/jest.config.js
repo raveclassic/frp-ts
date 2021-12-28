5996d3b7f1c0328714d76bc220727d1414aba577
raveclassic <raveclassic@gmail.com>
2021-12-28 22:09:26 +0300

    build: migrate to nx test pipeline

---
+++ packages/core/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	testRunner: 'jasmine2',
+	displayName: 'core',
+	preset: '../../jest.preset.js',
+	globals: {
+		'ts-jest': {
+			tsconfig: '<rootDir>/tsconfig.spec.json',
+		},
+	},
+	transform: {
+		'^.+\\.[tj]s$': 'ts-jest',
+	},
+	moduleFileExtensions: ['ts', 'js', 'html'],
+	coverageDirectory: '../../coverage/packages/core',
+}
