2099db880e67bf22bafe590d7b245eb65c086d7e
Dmitry Petrov <dvpetrov.main@gmail.com>
2022-01-13 16:38:09 +0300

    feat(lens): migrate to new signature

--- packages/lens/src/lensed-atom.ts
+++ packages/lens/src/lensed-atom.ts
@@ -1,4 +1,4 @@
-import { Atom, Env, atom } from '@frp-ts/core'
+import { Atom, Env, atom, interopObservable } from '@frp-ts/core'
 
 export interface Lens<S, A> {
 	readonly get: (s: S) => A
@@ -15,11 +15,13 @@ export const newLensedAtom = (env: Env): (<A>(initial: A) => LensedAtom<A>) => {
 }
 
 export const toLensedAtom = <A>(atom: Atom<A>): LensedAtom<A> => {
-	const { set, get, subscribe, modify } = atom
+	const { set, get, subscribe } = atom
 
 	const view = <B>(lens: Lens<A, B>): LensedAtom<B> => {
 		const lensedGet = () => lens.get(get())
 		const lensedSet = (b: B) => set(lens.set(b)(get()))
+		const interop = () => interopObservable.newInteropObservable(subscribe, lensedGet)
+
 		return {
 			get: lensedGet,
 			set: lensedSet,
@@ -32,14 +34,12 @@ export const toLensedAtom = <A>(atom: Atom<A>): LensedAtom<A> => {
 				}
 				lensedSet(value)
 			},
+			[interopObservable.observableSymbol]: interop,
 		}
 	}
 
 	return {
-		set,
-		get,
-		subscribe,
-		modify,
+		...atom,
 		view,
 	}
 }
