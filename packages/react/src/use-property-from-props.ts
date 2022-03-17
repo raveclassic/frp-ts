70dac4d06040f9aa1eca52d38f2da9bbe25bf6d0
Dmitry Petrov <dvpetrov.main@gmail.com>
2022-03-17 11:19:42 +0300

    fix(react): resolve bad `setState` in `usePropertyFromProps`

--- packages/react/src/use-property-from-props.ts
+++ packages/react/src/use-property-from-props.ts
@@ -1,8 +1,8 @@
 import { newAtom, Property } from '@frp-ts/core'
-import { useState } from 'react'
+import { useEffect, useMemo } from 'react'
 
 export const usePropertyFromProps = <Value>(value: Value): Property<Value> => {
-	const [state] = useState(() => newAtom(value))
-	state.set(value)
-	return state
+	const atom = useMemo(() => newAtom(value), [])
+	useEffect(() => atom.set(value), [value])
+	return atom
 }
