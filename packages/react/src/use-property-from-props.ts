0b2abed1257fec0690ac37e48498ef10b1c51f56
Dmitry Petrov <dvpetrov.main@gmail.com>
2022-03-18 12:03:31 +0300

    fix(react): `usePropertyFromProps` replace `effect` with `layoutEffect`

--- packages/react/src/use-property-from-props.ts
+++ packages/react/src/use-property-from-props.ts
@@ -1,8 +1,8 @@
 import { newAtom, Property } from '@frp-ts/core'
-import { useEffect, useMemo } from 'react'
+import { useLayoutEffect, useMemo } from 'react'
 
 export const usePropertyFromProps = <Value>(value: Value): Property<Value> => {
 	const atom = useMemo(() => newAtom(value), [])
-	useEffect(() => atom.set(value), [value])
+	useLayoutEffect(() => atom.set(value), [value])
 	return atom
 }
