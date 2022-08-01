710f03f8957488e2a25027142c102b17d86f5d88
Dmitry Petrov <dvpetrov.main@gmail.com>
2022-08-01 16:49:53 +0300

    feat(core): remove extra cache from interop

--- packages/core/src/interop-observable.ts
+++ packages/core/src/interop-observable.ts
@@ -25,17 +25,9 @@ export const newInteropObservable = <A>(
 	subscribe: Observable<Time>['subscribe'],
 ): InteropObservable<A> => ({
 	subscribe: (observer) => {
-		let hasValue = false
-		let value: A | undefined
 		const emitNext = () => {
 			if (observer.next) {
-				const prevValue = value
-				const prevHasValue = hasValue
-				value = get()
-				hasValue = true
-				if (!prevHasValue || prevValue !== value) {
-					observer.next(value)
-				}
+				observer.next(get())
 			}
 		}
 		emitNext()
