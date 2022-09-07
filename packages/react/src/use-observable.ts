d41ee6ea7b5c34a64a6c1a073e0d5dd3c40dd042
Dmitry Petrov <dvpetrov.main@gmail.com>
2022-09-07 16:46:10 +0300

    feat(react): use `useSyncExternalStore` for `Observable`

--- packages/react/src/use-observable.ts
+++ packages/react/src/use-observable.ts
@@ -1,19 +1,36 @@
 import { Observable } from '@frp-ts/core'
-import { useEffect, useState } from 'react'
+import { useRef, useCallback } from 'react'
+import { useSyncExternalStore } from 'use-sync-external-store/shim'
+
+interface Ref<Value> {
+	source: Observable<Value>
+	value: Value
+}
 
 export function useObservable<Value>(source: Observable<Value>, initial: Value): Value {
-	const [state, setState] = useState(() => initial)
-
-	useEffect(() => {
-		const subscription = source.subscribe({
-			next: (value) => {
-				setState(() => value)
-			},
-		})
-		return () => {
-			subscription.unsubscribe()
+	// eslint-disable-next-line no-restricted-syntax
+	const ref = useRef<Ref<Value>>(undefined as unknown as Ref<Value>)
+	if (ref.current === undefined || ref.current.source !== source) {
+		ref.current = {
+			source,
+			value: initial,
 		}
-	}, [source])
+	}
 
-	return state
+	const get = useCallback(() => ref.current.value, [])
+	const subscribe = useCallback(
+		(next: () => void) => {
+			const subscription = source.subscribe({
+				next: (value) => {
+					ref.current.value = value
+					next()
+				},
+			})
+
+			return subscription.unsubscribe
+		},
+		[source],
+	)
+
+	return useSyncExternalStore(subscribe, get)
 }
